b6c2512552e09eeb3ae9b2d30a379fa5a4163254
Allan Amaral <allanmaral@hotmail.com>
2019-10-03 13:23:08 -0300

    Simple placeholder login added with axios

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
-import './App.css';
+import api from './services/api';
 
-import logo from './assets/logo.svg'
+import './App.css';
+import logo from './assets/logo.svg' 
 
 function App() {
+  const [email, setEmail] = React.useState('');
+  
+  async function handleSubmit(e) {
+    e.preventDefault();
+    const response = await api.post('/sessions', { email });
+    const { _id } = response.data;
+    localStorage.setItem('user', _id);
+  }
+
   return (
     <div className="container">
       <img src={logo} alt="AirCnC Logo "/>
@@ -12,12 +22,14 @@ function App() {
         <p>
           Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para sua empresa
         </p>
-        <form>
+        <form onSubmit={handleSubmit}>
           <label htmlFor="email">E-MAIL *</label>
           <input 
             id="email" 
             type="email" 
-            placeholder="Seu melhor email" 
+            placeholder="Seu melhor email"
+            value={email}
+            onChange={ (e) => setEmail(e.target.value) }
           />
           <button className="btn" type="submit">Entrar</button>
         </form>
